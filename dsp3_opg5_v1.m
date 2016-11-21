b7bb49947ec4d9ef2984571b14c4baa1699dafc6
Kirial <jesperkirial@gmail.com>
2016-11-21 10:14:22 +0100

    Opgave formuleringer til opg.  3 4 5

--- dsp3_opg5_v1.m
+++ dsp3_opg5_v1.m
@@ -1,4 +1,11 @@
 %opgave 5
+
+% 5. Anvend en vindue-funktion på et af ovenstående signaler. 
+% Forklar hvorfor en vindue-funktion kan være nødvendig, 
+% hvad den gør og hvordan den anvendes.
+% Vis med et eksempel.
+
+
 function [] = dsp3_opg5_v1()
 
 6 + 5
