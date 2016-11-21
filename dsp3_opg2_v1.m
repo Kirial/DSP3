2992c2a6ca6c17b1ea89a027c07c74664c648778
Niklas Buhl <nibuh@dtu.dk>
2016-11-21 12:42:31 +0100

    Hej

--- dsp3_opg2_v1.m
+++ dsp3_opg2_v1.m
@@ -1,37 +1,50 @@
 % opgave2
 
+clear, close all
+
 % 2. Båndpas-filter til analyse af vokaler ’a’  ’e’  ’i’
 % Indspil selv lyd som indeholder vokalerne ’a’  ’e’  ’i’ – 
 % analyser signalerne og bestem de nødvendige filterspecifikationer 
 % så f.eks. et ’a’ detekteres i ’en talestrøm’. Implementer filteret 
 % i Matlab og dokumenter designet.
 
+%path('/Users/nb/Education/DTU/3-Semester/Signalbehandling/DSP3');
+
 
 %function [] = dsp3_opg2_v1()
 
 % Load lydfiler
 
-audio_name = 'audio_klip.wav';
-
-
+audio_test = '/Users/nb/Education/DTU/3-Semester/Signalbehandling/DSP3/audio_klip.wav';
 
 % Meta
 
 samplingRate = 48000; % [fs]
 
-sampleLength = 10; % [sek]
+samples = 187328; % [samples]
 
-timeVector = 0:1/samplingRate:sampleLength-1/samplingRate;
+%timeVector = 0:1/samplingRate:samples-1/samplingRate;
+
+sample_test = [1,samples];
 
 % Sampling
 
-
+[sound_test,samplingRate] = audioread(audio_test,sample_test);
 
 
 % Fourior Transformation - For at finde frekvens bånd for A,E,I
 
-%yA = fft(
+yTest = fft(sound_test);
 
+mTest = abs(yTest);
+
+pTest = unwrap(angle(yTest));
+
+fTest = (0:length(yTest)-1)*100/length(yTest);
+
+subplot(2,1,1);
+
+plot(fTest,mTest);
 
 % Bandpass - Filter
 
