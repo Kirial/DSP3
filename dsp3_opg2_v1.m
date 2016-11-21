b369eab6ccf7628d483e64e4c6238f9016a7b597
Niklas Buhl <nibuh@dtu.dk>
2016-11-21 10:59:06 +0100

    update

--- dsp3_opg2_v1.m
+++ dsp3_opg2_v1.m
@@ -4,6 +4,20 @@ function [] = dsp3_opg2_v1()
 
 % Bandpass - Filter
 
+lowerCutoffFrequency = 4000;
+
+upperCutoffFrequency = 5000;
+
+samplingRate = 8000;
+
+% --- Filter Coefficients ---
+
+Omega_low = 2 * pi * (lowerCutoffFrequency / samplingRate)
+
+Omega_up = 2 * pi * (upperCutoffFrequency / samplingRate)
+
+
+
 % "AAaaa..."
 
 % "IIiii..."
