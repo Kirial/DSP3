802986fb2fc87c1ac4b141fd012cda1b69dae297
Taras Karpin <tarass777@hotmail.com>
2016-11-23 10:40:50 +0100

    plot test opg 4

--- dsp3_opg4_v1.m
+++ dsp3_opg4_v1.m
@@ -6,9 +6,12 @@
 % Implementer filteret i Matlab og dokumenter designet.
 
 %function [] = dsp3_opg4_v1()
+load('nervesignal.mat');
+amplitude_EKG = cell2mat(nsVector3(:,1)');
+tid_EKG = cell2mat(nsVector3(:,2)');
 
-
-
+figure 
+plot(tid_EKG, amplitude_EKG);
 
 
 %end
