adcf13796a8cfa2d6c08dd66dc95376344d10f47
Taras Karpin <tarass777@hotmail.com>
2016-11-28 15:25:56 +0100

    opgave 4 færdig (gang 2)

--- dsp3_opg4_v1.m
+++ dsp3_opg4_v1.m
@@ -14,22 +14,39 @@ EKG_length = length(amplitude_EKG);
 f_sample = 96;
 tid_EKG = 0:(1/f_sample):((EKG_length/f_sample)-(1/f_sample));
 figure
+subplot(2,2,1)
 plot(tid_EKG,amplitude_EKG);
+xlabel('tid (s)')
+ylabel('amplitude (mV)')
 
 %fft
 EKG_fft = fft(amplitude_EKG);
 pos_fft = abs(EKG_fft);
 plot_signal = pos_fft(1:EKG_length/2+1);
 EKG_x =  (f_sample)*(0:(EKG_length/2))/EKG_length;
-figure
+subplot(2,2,3)
 plot(EKG_x,plot_signal);
+xlabel('Frekvens (Hz)')
+ylabel('Magnitude')
 
 this_signal = filter(opg4_filter, amplitude_EKG);
-figure
+subplot(2,2,2)
 plot(tid_EKG,this_signal);
+xlabel('tid (s)')
+ylabel('amplitude (mV)')
 this_signal_fft = fft(this_signal);
 P4 = abs(this_signal_fft);
 P3 = P4(1:EKG_length/2+1);
-figure
+subplot(2,2,4)
 plot(EKG_x,P3);
+xlabel('Frekvens (Hz)')
+ylabel('Magnitude')
+
+figure 
+plot(tid_EKG,amplitude_EKG);
+hold
+plot(tid_EKG,this_signal);
+xlabel('tid (s)')
+ylabel('amplitude (mV)')
+legend('Original EKG','Filtreret EKG');
 % %end
